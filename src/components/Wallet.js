35b48e92ee4daa16e16c2d3d73a71ab48b501608
ankitprasad <ap6112560@gmail.com>
2019-09-05 09:43:33 +0530

    Refactor. Add styling

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -3,10 +3,23 @@ import React from 'react';
 const Wallet = () => {
     return (
         <div>
-            <p>name</p>
-            <h4>Balance : ₹ 500.00</h4>
-            <button className="button" id="b1">Add money</button>
-            <button className="button" id="b2">Send money</button>
+            <div>
+                <div className="fixed-header">
+                    <div className="container">
+                        <nav>
+                            <p>Ankit Prasad</p>
+                        </nav>
+                    </div>
+                </div>
+                <div className="container">
+                    <h2>Balance : ₹ 500.00</h2>
+                    <button className="button" id="b1">Add money</button>
+                    <button className="button" id="b2">Send money</button>
+                </div>
+                <div className="fixed-footer">
+                    <div className="container"></div>
+                </div>
+            </div>
         </div>
     );
 };
