e61bc4e1e9d66caf741afd8f0a513199c42bd2cb
Arun Tw <4424176-twarun1@users.noreply.gitlab.com>
2019-09-06 21:56:28 +0530

    [Arun] Fix. Temporarily fix localhost and herokuapp api connectivity

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,5 +1,5 @@
 const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
-    app.use(proxy('/api', { target: process.env.REACT_APP_WALLET_API_URL?process.env.REACT_APP_WALLET_API_URL:"http://localhost:8080/" }));
+    app.use(proxy('/api', { changeOrigin: true, target: process.cwd().indexOf('/Users/')>-1?"http://localhost:8080/":"https://megamindswallet.herokuapp.com" }));
  };
