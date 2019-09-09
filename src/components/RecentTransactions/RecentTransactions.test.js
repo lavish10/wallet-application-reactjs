5384deab62e87d2bfdfd0bb6547ebe079d03c9f2
Hariharan Sivakumar <harishsivakumar97@gmail.com>
2019-09-09 11:32:30 +0530

    [Harish/Arun] Fix. Coverage issues with the recent transactions

--- src/components/RecentTransactions/RecentTransactions.test.js
+++ src/components/RecentTransactions/RecentTransactions.test.js
@@ -6,7 +6,7 @@ import RecentTransactions from "./RecentTransactions";
 jest.mock('axios');
 jest.mock('../../service/RecentTransactionService');
 describe('RecentTransactionTest', function () {
-    it('should ', async () => {
+    it('should be able to send get request for recent transactions ', async () => {
         const arrayOfTransactions = [{
             id: 1,
             type: "CREDIT",
@@ -19,5 +19,22 @@ describe('RecentTransactionTest', function () {
         await Promise.resolve();
 
         expect(RecentTransactionService.get).toHaveBeenCalledTimes(1);
+    });
+
+    it('should be able to display no transactions if no transaction happen', function () {
+        const app = shallow(<RecentTransactions transactions = {[]}/>);
+        expect(app.find('label').text()).toEqual('No recent transactions');
+    });
+
+    it('should be able to render recent transactions view if there is a transaction happen', function () {
+        const arrayOfTransactions = [{
+            id: 1,
+            type: "CREDIT",
+            amount: 50,
+            remarks: null,
+            createdAt: "2019-09-07T05:08:15.630+0000"
+        }];
+        const app = shallow(<RecentTransactions transactions ={arrayOfTransactions}/>);
+        expect(app.find('RecentTransactionsView')).toHaveLength(1);
     });
 });
--- src/components/RecentTransactions/RecentTransactions.view.test.js
+++ src/components/RecentTransactions/RecentTransactions.view.test.js
@@ -17,7 +17,7 @@ describe('RecentTransactionViewTest', function () {
         expect(app.find(BootstrapTable)).toHaveLength(1);
     });
 
-    it('should be able to render the table without border', function () {
+    it('should be able to render the table for credit', function () {
         const transactions = [{
             id:1,
             remarks: null,
@@ -31,7 +31,19 @@ describe('RecentTransactionViewTest', function () {
         expect(dataTable.props().keyField).toEqual('id');
     });
 
-
+    it('should be able to render the table for debit', function () {
+        const transactions = [{
+            id:1,
+            remarks: null,
+            type: "DEBIT",
+            amount: 50,
+            createdAt: "2019-09-07T05:08:15.630+0000"
+        }];
+        const app = shallow(<RecentTransactionsView transactions={transactions}/>);
+        const dataTable = app.find(BootstrapTable);
+        dataTable.render();
+        expect(dataTable.props().keyField).toEqual('id');
+    });
 
 
 });
