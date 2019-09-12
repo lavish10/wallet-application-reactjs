0d1a3eef65e5335ca0d926b21bbd08a6583dadcc
lavish.yadav <lavishyadav@live.com>
2019-09-12 10:23:03 +0530

    [Hari/Lavish] Refactor. Date Filter test

--- src/components/Wallet/DateFilter.test.js
+++ src/components/Wallet/DateFilter.test.js
@@ -1,15 +1,16 @@
-import WalletView from "./Wallet.view";
 import {shallow} from "enzyme";
 import React from "react";
-import Header from "../Header";
 import DateFilter from "./DateFilter";
+import {FormCheck, FormGroup} from "react-bootstrap";
 
 describe('DateFilter', function () {
     it('should display 3 radio buttons ', function () {
-        const wallet = shallow(<DateFilter id={1}/>);
-        const radioButtons = wallet.find('Radio');
+        const wallet = shallow(<DateFilter/>);
+        const container = wallet.find('Container');
+        const formGroup = container.find('FormGroup');
+        const formCheck = formGroup.find('FormCheck');
 
-        expect(radioButtons).toHaveLength(3);
+        expect(formCheck).toHaveLength(3);
     });
 
     it('should display 1 search button ', function () {
@@ -19,4 +20,4 @@ describe('DateFilter', function () {
         expect(Button).toHaveLength(1);
     });
 
-})
+});
